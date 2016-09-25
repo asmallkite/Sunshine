9d51e8a93c6a62f55caa94986694fa4eb347dd16
LiZheng <lixiaozheng.good@163.com>
2016-09-25 16:36:01 -0700

    add location prefs

--- app/src/main/java/com/example/lizheng/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/lizheng/sunshine/ForecastFragment.java
@@ -2,9 +2,11 @@ package com.example.lizheng.sunshine;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
@@ -104,7 +106,10 @@ public class ForecastFragment extends Fragment {
             Toast.makeText(getActivity(), "Refresh", Toast.LENGTH_SHORT).show();
             Log.d("Fragment", "refresh");
             FetchWeatherTask weatherTask =new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             Log.d("Fragment", "execute");
             return true;
         }
